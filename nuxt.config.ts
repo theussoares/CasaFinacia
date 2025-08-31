a036810ec1bba98f596c2d14b16d0f50f13819b5
Matheus Ferreira Soares <matheusfs774@gmail.com>
2025-08-31 09:05:32 -0400

    feat: removendo vite config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,12 +27,4 @@ export default defineNuxtConfig({
       viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
     }
   },
-
-  vite: {
-    resolve: {
-      alias: {
-        vue: 'vue/dist/vue.esm-bundler.js'
-      }
-    }
-  }
 })
